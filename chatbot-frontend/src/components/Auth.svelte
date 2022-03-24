871b849560f32e55e2d6292cf26d606446a62648
Irate-Walrus <71628949+Irate-Walrus@users.noreply.github.com>
2022-03-24 17:10:34 +1100

    chore: template project

---
+++ chatbot-frontend/src/components/Auth.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import supabase from '$lib/supabase';
+
+	let email: string;
+
+	const handleLogin = async () => {
+		console.log(email);
+		const { error } = await supabase.auth.signIn({ email });
+		if (error) throw error;
+	};
+</script>
+
+<form on:submit|preventDefault={handleLogin}>
+	<div class="flex flex-col test-sm mb-2">
+		<label class="font-bold mb-2 text-blue-500" for="email">Email</label>
+		<input
+			class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+			type="email"
+			placeholder="Your email"
+			bind:value={email}
+		/>
+	</div>
+	<button
+		class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 test-white py-2 px-4"
+		type="submit"
+	>
+		Log In
+	</button>
+</form>
---
+++ chatbot-frontend/src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Auth from '../components/Auth.svelte';
+</script>
+
+<h1 class="font-bold h1 mb-2 text-blue-500">Welcome to SvelteKit</h1>
+<Auth />
